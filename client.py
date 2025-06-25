89e8e63458c56d005a350322dbfeb61282a7c632
Jagroop Singh <108449470+jay-samra@users.noreply.github.com>
2025-06-25 13:17:32 -0700

    client implementation, added quit command, TCP socket

--- client.py
+++ client.py
@@ -6,3 +6,34 @@ def main():
     if len(sys.argv != 3):
         print("Usage: python3 server.py <server_ip> <server_port>")
         sys.exit(1)
+        
+    # storing the input 
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+    
+    # creating socket
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # connecting to the server
+    clientSocket.connect((host ,port))
+    
+    # User must user JOIN to enter chat
+    print("Please enter JOIN followed by name: ")
+    newUser = input()
+    
+    clientSocket.send(newUser.encode())
+    
+    while True:
+        userCommand = input()
+        clientSocket.send(userCommand.encode())
+        if userCommand == "QUIT":
+            break
+        
+    clientSocket.close()
+    
+if __name__ == "__main__":
+    main()
+    
+    
+    
+        
