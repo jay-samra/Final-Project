53118e9edbc7692e24cb668254f8b84d433d2bd0
Jagroop Singh <108449470+jay-samra@users.noreply.github.com>
2025-06-27 22:58:47 -0700

    clearer documentation + clean-up

--- client.py
+++ client.py
@@ -1,19 +1,28 @@
+# Abhijit Singh Ubhi, Jagmeet Singh, Jagroop Singh
+# Dr. Syed Badruddoja
+# Final Project
+
 import sys
 import socket
 import threading
 
 
 def main():
+    # ensuring that the user has enough arguments in their client call
     if len(sys.argv) != 3:
         print("Usage: python3 server.py <server_ip> <server_port>")
         sys.exit(1)
         
+    # socket object is passed as a argument 
     def reciever(socket):
+        # function always recieved data from socket
         while True:
             try:
+                # recieving data from the socket
                 data = socket.recv(1024)
                 if not data:
                     break
+                # displaying the data from the socket
                 print("Recieved from server: ", str(data.decode()))
             except:
                 break
@@ -28,12 +37,14 @@ def main():
     # connecting to the server
     clientSocket.connect((host ,port))
     
+    # using threaing function to create threads which allow user to carry  out multiple actions
     threading.Thread(target=reciever, args=(clientSocket,)).start()
     
     # User must user JOIN to enter chat
     print("Please enter JOIN followed by name: ")
     newUser = input()
     
+    # sending the name of the new user
     clientSocket.send(newUser.encode())
     
     
@@ -41,9 +52,11 @@ def main():
     while True:
         userCommand = input()
         clientSocket.send(userCommand.encode())
+        # exiting program 
         if userCommand == "QUIT":
             break
         
+    # always closing the socket
     clientSocket.close()
     
 if __name__ == "__main__":
